b23a218d84e55cf7807b27293e23463be691b757
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-05-25 00:02:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    navSpeed: 4000,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
